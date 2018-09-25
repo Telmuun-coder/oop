7f6fb516c48450de2e5a3129d40065c5cbe43f93
Samdan <pri.samdan@gmail.com>
2018-09-26 03:51:57 +0800

    Finished docs

--- lab3/employee.h
+++ lab3/employee.h
@@ -1,30 +1,28 @@
 #include <iostream>
 using namespace std;
+class employee
+{
+  public:
+	// Гишүүн ажилчны дугаар
+	int id;
+	// Гишүүн ажилчны нэр
+	char name[20];
+	// Гишүүн ажилчны албан тушаал
+	char position[20];
+	// Гишүүн ажилчны ажилсан цаг
+	float worked_hour;
 
-class employee {
-    public:
-        // Гишүүн ажилчны дугаар
-        int id;
-        // Гишүүн ажилчны нэр
-        char name[20];
-        // Гишүүн ажилчны албан тушаал
-        char position[20];
-        // Гишүүн ажилчны ажилсан цаг
-        float worked_hour;    
-
-    public:
-        // Гишүүн ажилчинд анхны утга оноож үүсгэнэ.
-        void init();
-        // Гишүүн ажилчны мэдээлэл дэлгэцэнд хэвлэх
-        void print();
-        // Гишүүн ажилчны мэдээлэл
-        void read();
-        // Гишүүн ажилчны цалинг ажилласан цагаар нь бодож буцаана.
-        float calculate_salary();
-        // Захирлын цалинг бодох
-        float calculate_salary_ceo();
-        // Гишүүн ажилчны ажилласан цагийг нэмнэ
-        bool add_worked_hour(float hour);
-
-    // Do g++ -c *.cpp -o employee.o g++ main.cpp employee.o ./main.exe };
+  public:
+	// Гишүүн ажилчинд анхны утга оноож үүсгэнэ.
+	void init();
+	// Гишүүн ажилчны мэдээлэл дэлгэцэнд хэвлэх
+	void print();
+	// Гишүүн ажилчны мэдээлэл
+	void read();
+	// Гишүүн ажилчны цалинг ажилласан цагаар нь бодож буцаана.
+	float calculate_salary();
+	// Захирлын цалинг бодох
+	float calculate_salary_ceo();
+	// Гишүүн ажилчны ажилласан цагийг нэмнэ
+	bool add_worked_hour(float hour);
 };
